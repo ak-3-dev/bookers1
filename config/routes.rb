b053c4593bea3afcb5e2ca66310c4f16588e7f01
Ubuntu <ubuntu@ip-172-31-3-173.ap-northeast-1.compute.internal>
2026-03-21 14:02:54 +0000

    fix: lint & test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "books/index"
   get "books/show"
   get "books/edit"
-  get "/top" => "homes#top"
+  get "top", to: "homes#top", as: "homes_top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
