df36a9e9792179278774455dd7227aceb1c80207
Ubuntu <ubuntu@ip-172-31-3-173.ap-northeast-1.compute.internal>
2026-03-25 15:31:20 +0000

    [bookers]edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,12 @@ class BooksController < ApplicationController
     
   end
 
+  def show 
+    @book = Book.find(params[:id])
+  end
+
   def edit
+    @book = Book.find(params[:id])
   end
 
   private
